4702d1f9e33297d3cb8a55c5990de83338a007c3
Yousri <yhajri1976@gmail.com>
2017-06-14 10:48:56 +0100

    edit cachematrix for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## This function creates a special "matrix" object that can cache
 ## its inverse
 
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
     x <<- y
